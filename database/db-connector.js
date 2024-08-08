8521b3a7f663a89081aab02ac5599d375a6ab74c
Your Name <stacyz@oregonstate.edu>
2024-08-08 14:53:13 -0700

    with db-connector reflecting wei data

--- database/db-connector.js
+++ database/db-connector.js
@@ -5,10 +5,20 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_stacyz',
-    password        : '5296',
-    database        : 'cs340_stacyz'
+    user            : 'cs340_hew2',
+    password        : '4977',
+    database        : 'cs340_hew2'
 })
+
+
+// // Create a 'connection pool' using the provided credentials
+// var pool = mysql.createPool({
+//     connectionLimit : 10,
+//     host            : 'classmysql.engr.oregonstate.edu',
+//     user            : 'cs340_stacyz',
+//     password        : '5296',
+//     database        : 'cs340_stacyz'
+// })
 
 // Export it for use in our application
 module.exports.pool = pool;
