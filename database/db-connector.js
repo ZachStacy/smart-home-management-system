72da7fd566c99b15c24353529e5f83139a2f9165
Wei He <hew2@oregonstate.edu>
2024-08-09 17:14:51 -0700

    Merge

--- database/db-connector.js
+++ database/db-connector.js
@@ -4,7 +4,6 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_stacyz',
     password        : '5296',
